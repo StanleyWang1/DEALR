c009cbe942096fd1abbf9fa58f087636ff42804d
Bryan Ngo <bryanngo@berkeley.edu>
2025-07-27 21:00:17 -0700

    implement dealer logic

--- src/blackjack/dealer.py
+++ src/blackjack/dealer.py
@@ -1,4 +1,5 @@
 """FSM for a blackjack dealer."""
+
 import itertools
 import random
 
@@ -25,13 +26,16 @@ class Dealer(StateMachine):
     idle = State(initial=True)
     initial_deal = State()
     waiting_for_player = State()
-    resolving = State(final=True)
+    resolving_dealer = State()
+    done = State(final=True)
 
     start_game = idle.to(initial_deal)
     player_action_start = initial_deal.to(waiting_for_player)
-    player_hits = waiting_for_player.to(waiting_for_player)
-    player_stands = waiting_for_player.to(waiting_for_player)
-    resolve_dealer_hand = waiting_for_player.to(resolving, cond="all_players_done", final=True)
+    player_hits = waiting_for_player.to.itself()
+    player_stands = waiting_for_player.to.itself()
+    player_blackjack = waiting_for_player.to(done, cond="player_has_blackjack")
+    resolve_dealer_hand = waiting_for_player.to(resolving_dealer, cond="all_players_done")
+    settling_bets = resolve_dealer_hand.to(done)
 
     def __init__(self, players: list[Player]) -> None:
         self.players = players
@@ -41,11 +45,18 @@ class Dealer(StateMachine):
         super().__init__()
 
     def all_players_done(self) -> bool:
-        all_standing = all(p.last_action == PlayerAction.STAND for p in self.players if p.status == PlayerStatus.ACTIVE)
+        all_standing = all(
+            p.last_action == PlayerAction.STAND
+            for p in self.players
+            if p.status == PlayerStatus.ACTIVE
+        )
         all_busted = all(p.status == PlayerStatus.BUSTED for p in self.players)
         return all_standing or all_busted
-    
-    def before_start_game(self) -> None:
+
+    def player_has_blackjack(self) -> bool:
+        return any(cards.hand_value(p.hand) == 21 for p in self.players)
+
+    def on_start_game(self) -> None:
         random.shuffle(self.deck)
         # TODO: add hardware shuffling
         for _ in range(2):
@@ -54,9 +65,6 @@ class Dealer(StateMachine):
                 player.hand.append(card)
             dealer_card = self.deck.pop()
             self.hand.append(dealer_card)
-        
-    def on_exit_waiting_for_player(self) -> None:
-        self.player_index = (self.player_index + 1) % len(self.players)
 
     def on_player_hits(self) -> None:
         player = self.players[self.player_index]
@@ -65,12 +73,35 @@ class Dealer(StateMachine):
         player.last_action = PlayerAction.HIT
         if cards.hand_value(player.hand) > 21:
             player.status = PlayerStatus.BUSTED
+            player.bet = 0
+        self.player_index = (self.player_index + 1) % len(self.players)
 
     def on_player_stands(self, player: Player) -> None:
         player.last_action = PlayerAction.STAND
+        self.player_index = (self.player_index + 1) % len(self.players)
 
     def on_resolve_dealer_hand(self) -> None:
         while dealer_hand_value(self.hand) < 17:
             card = self.deck.pop()
             self.hand.append(card)
-        # TODO: settle bets
+    
+    def on_player_blackjack(self) -> None:
+        active_players = [p for p in self.players if p.status == PlayerStatus.ACTIVE]
+        if cards.hand_value(self.hand) != 21:
+            for p in active_players:
+                if cards.hand_value(p.hand) == 21:
+                    p.bet = p.bet + p.bet // 2
+
+    def on_settling_bets(self) -> None:
+        dealer_value = dealer_hand_value(self.hand)
+        active_players = [p for p in self.players if p.status == PlayerStatus.ACTIVE]
+        if dealer_value > 21:
+            for p in active_players:
+                p.bet *= 2
+        else:
+            for p in active_players:
+                player_value = cards.hand_value(p.hand)
+                if player_value > dealer_value:
+                    p.bet *= 2
+                elif player_value < dealer_value:
+                    p.bet = 0
